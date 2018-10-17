38d06fe453a053e0d320161da3bde97152e1cf01
jenchieh <jcs090218@gmail.com>
2018-10-17 22:50:16 +0800

    Add enum unit.

---
+++ src/enum/JCS_2DAnimData.ts
@@ -0,0 +1,28 @@
+/**
+ * $File: JCS_2DAnimData.ts $
+ * $Date: 2018-10-17 21:34:01 $
+ * $Revision: $
+ * $Creator: Jen-Chieh Shen $
+ * $Notice: See LICENSE.txt for modification and distribution information
+ *                   Copyright © 2018 by Shen, Jen-Chieh $
+ */
+
+
+namespace JCSEgret {
+    /**
+     * @desc List of animation data.
+     */
+    export enum JCS_2DAnimData {
+        X,
+        Y,
+
+        WIDTH,
+        HEIGHT,
+
+        PIVOT_X,
+        PIVOT_Y,
+
+        SCALE_X,
+        SCALE_Y,
+    }
+}
