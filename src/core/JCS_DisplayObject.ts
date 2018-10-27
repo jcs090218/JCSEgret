a4bba49de40aa817a8164028dbf204ecb8096f9d
jenchieh <jcs090218@gmail.com>
2018-10-28 00:02:37 +0800

    Add display object interface.

---
+++ src/core/JCS_DisplayObject.ts
@@ -0,0 +1,24 @@
+/**
+ * $File: JCS_DisplayObject.ts $
+ * $Date: 2018-10-27 23:05:55 $
+ * $Revision: $
+ * $Creator: Jen-Chieh Shen $
+ * $Notice: See LICENSE.txt for modification and distribution information
+ *                   Copyright © 2018 by Shen, Jen-Chieh $
+ */
+
+
+namespace JCSEgret {
+
+    /**
+     * Display object.
+     */
+    export abstract class JCS_DisplayObject {
+
+        /**
+         * @desc Add this to the layer/interface/scene that
+         * would display this object.
+         */
+        public abstract addToDOC(doc : egret.DisplayObjectContainer) : void;
+    }
+}
