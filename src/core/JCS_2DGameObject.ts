756bf5894910704a5b55df50e48d9001369e52d8
jenchieh <jcs090218@gmail.com>
2018-10-28 00:03:50 +0800

    Impl gameobject.

--- src/core/JCS_2DGameObject.ts
+++ src/core/JCS_2DGameObject.ts
@@ -13,12 +13,79 @@ namespace JCSEgret {
     /**
      * @desc GameObject determine the status of this object.
      */
-    export class JCS_2DGameObject {
+    export class JCS_2DGameObject extends JCS_Component {
 
+        private _components : JCS_Component[] = new Array();
+
+
+        /* Setter/Getter */
+
+
+        /**
+         * @param main Container that can be register the update event to.
+         */
         public constructor() {
-            // empty..
+            super();
         }
 
+        /**
+         * @desc Update is called each frame.
+         */
+        public update() : void {
+            if (!this.active)
+                return;
 
+            /* Update all the components. */
+            this._components.forEach(function (comp) {
+                comp.update();
+            });
+        }
+
+        /**
+         * @desc Add a component to this gameobject.
+         *
+         * @param comp Component to add to this gameobject.
+         * @returns Component id represent to this component.
+         */
+        public addComp(comp : JCS_Component) : number {
+            if (comp == null) {
+                JCS_Debug.error("Cannot add component with null reference...");
+                return;
+            }
+
+            this._components.push(comp);
+
+            // Returns component's id.
+            let compId : number = this._components.length - 1;
+            return compId;
+        }
+
+        /**
+         * @desc Remove the component by id.
+         *
+         * @param id Component id.
+         */
+        public removeCompById(id : number) : void {
+            delete this._components[id];
+        }
+
+        /**
+         * @desc Get the component by component id.
+         *
+         * @param id Component id.
+         */
+        public getCompById(id : number) : JCS_Component {
+            return this._components[id];
+        }
+
+        /**
+         * @desc Add this to the layer/interface/scene that
+         * would display this object.
+         */
+        public addToDOC(doc : egret.DisplayObjectContainer) : void {
+            this._components.forEach(function (comp) {
+                comp.addToDOC(doc);
+            });
+        }
     }
 }
