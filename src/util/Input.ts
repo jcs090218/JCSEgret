9e2dc5b9768bd514b3165101759cb7fac6ad01d7
jenchieh <jcs090218@gmail.com>
2018-10-29 02:01:22 +0800

    Add input module.

---
+++ src/util/Input.ts
@@ -0,0 +1,138 @@
+/**
+ * $File: Input.ts $
+ * $Date: 2018-10-28 22:09:50 $
+ * $Revision: $
+ * $Creator: Jen-Chieh Shen $
+ * $Notice: See LICENSE.txt for modification and distribution information
+ *                   Copyright © 2018 by Shen, Jen-Chieh $
+ */
+
+
+namespace JCSEgret {
+
+    /**
+     * @desc Input handle.
+     */
+    export class Input {
+
+        private static _keysPressedThisFrame : KeyCode[] = [];
+        private static _keysReleaseThisFrame : KeyCode[] = [];
+        private static _keysDown : KeyCode[] = [];
+
+
+        /**
+         * @desc Initialize for input module to get ready to work.
+         */
+        public static init() : void {
+            Input.initKeyboard();
+        }
+
+        /**
+         * @desc Clean the input buffer every frame.
+         */
+        public static cleanInputBuffer() : void {
+            Input._keysDown = [];
+            Input._keysReleaseThisFrame = [];
+        }
+
+        /**
+         * @desc Check if the key is down?
+         * @param keyCode Key code.
+         * @returns True, key is down. False, key is not down.
+         */
+        public static getKeyDown(keyCode : KeyCode) : boolean {
+            for (let index = 0;
+                 index < Input._keysDown.length;
+                 ++index)
+            {
+                let key = Input._keysDown[index];
+                if (key == keyCode) {
+
+                    // Check contains.
+                    if (Input._keysPressedThisFrame.indexOf(key) > -1) {
+                        return false;
+                    } else {
+                        // The key is down this frame, add to the check list.
+                        // So when the next time it etners will return false.
+                        Input._keysPressedThisFrame.push(key);
+
+                        return true;
+                    }
+                }
+
+            }
+            return false;
+        }
+
+        /**
+         * @desc Check if the key is held down?
+         * @param keyCode Key code.
+         * @returns True, key is held down. False, key is not held down.
+         */
+        public static getKey(keyCode : KeyCode) : boolean {
+            for (let index = 0;
+                 index < Input._keysDown.length;
+                 ++index)
+            {
+                let key = Input._keysDown[index];
+                if (key == keyCode)
+                    return true;
+            }
+            return false;
+        }
+
+        /**
+         * @desc Check if the key is up?
+         * @param keyCode Key code.
+         * @returns True, key is up. False, key is not up.
+         */
+        public static getKeyUp(keyCode : KeyCode) : boolean {
+            for (let index = 0;
+                 index < Input._keysReleaseThisFrame.length;
+                 ++index)
+            {
+                let key = Input._keysReleaseThisFrame[index];
+                if (key == keyCode)
+                    return true;
+            }
+            return false;
+        }
+
+        /**
+         * @desc Initialize the keyboard receiver.
+         * @param keyCode Key code.
+         * @returns True, key is up. False, key is not up.
+         */
+        private static initKeyboard() {
+            // Keyboard handle.
+            document.addEventListener("keydown", (e) => {
+                let key : KeyCode = <KeyCode>e.keyCode;
+
+                Input._keysDown.push(key);
+            });
+
+            document.addEventListener("keyup", (e) => {
+                let key : KeyCode = <KeyCode>e.keyCode;
+
+                Input._keysReleaseThisFrame.push(key);
+
+                // Remove the key down list.
+                Input.removeKeyFromList(Input._keysPressedThisFrame, key);
+            });
+        }
+
+        /**
+         * @desc Remove a key from a list.
+         * @param list List of keycode.
+         * @param key Target key code you want to remove.
+         */
+        private static removeKeyFromList(list : KeyCode[], key : KeyCode) {
+            const searchIndex = list.indexOf(key, 0);
+            if (searchIndex > -1) {
+                // Remove it.
+                list.splice(searchIndex, 1);
+            }
+        }
+
+    }
+}
