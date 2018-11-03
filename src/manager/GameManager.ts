a7a2085edfbfa90b3db74fc4e98696bb2007bc99
jenchieh <jcs090218@gmail.com>
2018-11-03 19:35:07 +0800

    Add main loop holder.

--- src/manager/GameManager.ts
+++ src/manager/GameManager.ts
@@ -11,14 +11,17 @@
 namespace JCSEgret {
 
     /**
-     * @desc Game manager.
+     * @desc Game manager, handle the game behaviours.
      */
     export class GameManager {
 
         private static instance : GameManager = new GameManager();
 
+        // Hold the main layer.
         private _layer : eui.UILayer = null;
 
+        // Main program loop.
+        private _mainLoop : () => void = null;
 
         /* setter/getter */
         public getLayer() : eui.UILayer { return this._layer; }
@@ -36,15 +39,43 @@ namespace JCSEgret {
         }
 
         /**
-         * Initialize the game manager.
+         * @desc Initialize the game manager.
          * @param layer Layer the program entrance.
          */
-        public init(layer : eui.UILayer) {
+        public init(layer : eui.UILayer) : void {
             this._layer = layer;
         }
 
+        /**
+         * @desc Update called every frame.
+         */
+        public update() : void {
+            this.processEvent();
+        }
+
+        /**
+         * @desc Set the main loop execution.
+         * @param func Main loop function pointer.
+         */
+        public setMainLoop(func : () => void) : void {
+            this._mainLoop = func;
+        }
+
+        /**
+         * @desc Add a display object to the layer.
+         */
         public addToLayer(child : egret.DisplayObject) {
             this.getLayer().addChild(child);
+        }
+
+        /**
+         * @desc Process the main loop.
+         */
+        private processEvent() : void {
+            if (this._mainLoop == null)
+                return;
+
+            this._mainLoop();
         }
     }
 }
