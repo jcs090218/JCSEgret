11a621968466f454ab3137160f3c9a413e28a0d9
jenchieh <jcs090218@gmail.com>
2018-11-03 17:15:35 +0800

    Add move to last child util.

--- src/util/Util.ts
+++ src/util/Util.ts
@@ -33,5 +33,27 @@ namespace JCSEgret {
             return val;
         }
 
+        /**
+         * @desc Move the display object to the last child of the render layer.
+         * @param dis Display object in the render layer.
+         * @param layer Layer that will remove `dis` and add the `dis` back to
+         * render queue.
+         */
+        public static moveToLastChild(dis : egret.DisplayObject, layer : eui.UILayer = null) : void {
+            let tmpLayer = layer;
+
+            if (dis == null)
+                return;
+
+            // If the layer is null, assign the global layer.
+            if (layer == null)
+                tmpLayer = GameManager.getInstance().getLayer();
+
+            // NOTE(jenchieh): By removing and adding the child will move the
+            // child to the last of the child index, which the render oder
+            // will be modified to the top of the scene.
+            tmpLayer.removeChild(dis);
+            tmpLayer.addChild(dis);
+        }
     }
 }
