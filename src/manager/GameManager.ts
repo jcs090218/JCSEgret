e6cbbc3b598b87e1fd6f16c50dda931b029fcf84
jenchieh <jcs090218@gmail.com>
2018-11-03 20:17:06 +0800

    Add reg/dereg multiple main loop instead of just one main loop.

--- src/manager/GameManager.ts
+++ src/manager/GameManager.ts
@@ -21,7 +21,7 @@ namespace JCSEgret {
         private _layer : eui.UILayer = null;
 
         // Main program loop.
-        private _mainLoop : () => void = null;
+        private _mainLoops : Array<() => void> = new Array();
 
         /* setter/getter */
         public getLayer() : eui.UILayer { return this._layer; }
@@ -54,28 +54,45 @@ namespace JCSEgret {
         }
 
         /**
-         * @desc Set the main loop execution.
-         * @param func Main loop function pointer.
-         */
-        public setMainLoop(func : () => void) : void {
-            this._mainLoop = func;
-        }
-
-        /**
          * @desc Add a display object to the layer.
+         * @param child Display object to add to.
          */
         public addToLayer(child : egret.DisplayObject) {
             this.getLayer().addChild(child);
         }
 
         /**
+         * @desc Register one main loop execution.
+         * @param func Main loop function pointer.
+         * @returns Program Id.
+         */
+        public registerMainLoop(func : () => void) : number {
+            if (func == null) {
+                Debug.log("Cannot register the main loop with null references...");
+                return -1;
+            }
+
+            this._mainLoops.push(func);
+
+            let programId = this._mainLoops.length - 1;
+            return programId;
+        }
+
+        /**
+         * @desc Deregister one main loop execution by program id.
+         * @param id Program id.
+         */
+        public deregisterMainLoop(id : number) {
+            delete this._mainLoops[id];
+        }
+
+        /**
          * @desc Process the main loop.
          */
         private processEvent() : void {
-            if (this._mainLoop == null)
-                return;
-
-            this._mainLoop();
+            this._mainLoops.forEach(function (func) {
+                func();
+            });
         }
     }
 }
